eec05b9f76088b5dcd295cc10866eaf695fad91b
Sarthak Kundra <sarthakkundra21@gmail.com>
2021-03-18 01:13:07 +0530

    Created layout for NFT card

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 
 import {Text} from "@chakra-ui/react";
+import Card from "./components/Card";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       >
         Welcome to Tokenized Videos!
       </Text>
+      <Card />
     </div>
   );
 }
