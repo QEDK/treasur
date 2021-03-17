69d1af10b8958dad80b14361836145c61ece8766
Sarthak Kundra <sarthakkundra21@gmail.com>
2021-03-18 00:27:46 +0530

    added dev dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
+import "./App.css";
 
-import './App.css';
+import {Text} from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="App">
-      
-      Tokenized Videos!!
+      <Text
+        bgGradient="linear(to-l, #7928CA,#FF0080)"
+        bgClip="text"
+        fontSize="6xl"
+        fontWeight="extrabold"
+      >
+        Welcome to Tokenized Videos!
+      </Text>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-import { ChakraProvider } from '@chakra-ui/react';
-
+import {ChakraProvider} from "@chakra-ui/react";
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-    <App />
+      <App />
     </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
