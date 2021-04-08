920656c21b3f679cb94d8219d070631689ab2986
Sarthak Kundra <sarthakkundra21@gmail.com>
2021-04-09 00:36:35 +0530

    starting redux setup

---
+++ client/src/store/reducerConfig.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+
+
+const reducer = combineReducers({
+
+});
+
+const store = configureStore({
+    reducer
+})
+
+export default store;
