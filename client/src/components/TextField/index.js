98b4fa0a86c545d9f623eb7b419e8e07b0f5bef5
Sarthak Kundra <sarthakkundra21@gmail.com>
2021-04-10 13:08:29 +0530

    added text field for YT url

--- client/src/components/TextField/index.js
+++ client/src/components/TextField/index.js
@@ -1,9 +1,22 @@
-import React from 'react'
-import { Input } from '@chakra-ui/react';
+import React, { useState } from 'react'
+import { Input, Button } from '@chakra-ui/react';
 const index = () => {
+    const [url, setUrl] = useState('');
+
+    const onChange = (e) => {
+        setUrl(e.target.value);
+    }
+
+    const onSubmit = () => {
+        // Call all the contract methods to check
+        // if the video has been offered, listed or minted
+        // if nothing out of these 3 then mint it.
+        console.log(url)
+    }
     return (
         <div>
-            <Input variant="flushed" placeholder="Enter Youtube URL here" focusBorderColor="red.300" width="180%"/>
+            <Input variant="flushed" placeholder="Enter Youtube URL here" focusBorderColor="red.300" width="180%" onChange={onChange}/>
+            <Button colorScheme="blue" onClick={onSubmit}>Mint</Button>
         </div>
     )
 }
