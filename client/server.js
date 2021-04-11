c7b522a5335b5b217ccf3343a3e456840d4d2462
QEDK <1994constant@gmail.com>
2021-04-11 07:56:40 +0530

    :new: Add Express backend

---
+++ client/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/ping', (req, res) => {
+  return res.send('pong')
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+app.listen(8080)
