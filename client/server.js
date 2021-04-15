f0c5b57f267e8cedc00a7d09cfe266b9695c6270
Sarthak Kundra <sarthakkundra21@gmail.com>
2021-04-15 22:41:35 +0530

    pushing buggy func

--- client/server.js
+++ client/server.js
@@ -1,6 +1,19 @@
+require('dotenv').config()
 const express = require('express')
+const Web3 = require('web3');
 const path = require('path')
 const app = express()
+// var web3 = new Web3('https://rpc-mainnet.maticvigil.com/');
+var web3 = new Web3 (new Web3.providers.HttpProvider('https://rpc-mainnet.maticvigil.com/'))
+const account = web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY)
+
+web3.eth.accounts.wallet.add(account);
+
+const Treasur = require('./src/config/Treasur.json')
+const TreasurContract = new web3.eth.Contract(
+  Treasur,
+  "0x54b327694d71596128064b495C70Fe9F0d2919ad"
+);
 
 app.use(express.static(path.join(__dirname, 'build')))
 
@@ -12,4 +25,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
+app.post('/mint', async (req, res) => {
+  try {
+    console.log(req.body);
+    const rv = await TreasurContract.methods.approveMint(req.tokenUri, req.tokenURIStr, req.tokenCreator).send({"from": "0xd1058ECCEE8102Bb8C1A7390b7d6Ea2CB6dA8E0e"})
+    res.send(rv);
+  } catch (e) {
+    console.log('This request is fucking up')
+  }
+
+})
 app.listen(8080)
