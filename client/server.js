c394ec07ee492e2b51a3b20161d80d5452fd9a27
Sarthak Kundra <sarthakkundra21@gmail.com>
2021-04-16 14:42:39 +0530

    fixed undefined error, found new error

--- client/server.js
+++ client/server.js
@@ -1,38 +1,42 @@
-require('dotenv').config()
-const express = require('express')
-const Web3 = require('web3');
-const path = require('path')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const Web3 = require("web3");
+const path = require("path");
+const app = express();
 // var web3 = new Web3('https://rpc-mainnet.maticvigil.com/');
-var web3 = new Web3 (new Web3.providers.HttpProvider('https://rpc-mainnet.maticvigil.com/'))
-const account = web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY)
+var web3 = new Web3(
+  new Web3.providers.HttpProvider("https://rpc-mainnet.maticvigil.com/")
+);
+const account = web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY);
 
 web3.eth.accounts.wallet.add(account);
 
-const Treasur = require('./src/config/Treasur.json')
+const Treasur = require("./src/config/Treasur.json");
 const TreasurContract = new web3.eth.Contract(
   Treasur,
   "0x54b327694d71596128064b495C70Fe9F0d2919ad"
 );
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "build")));
 
-app.use(express.static(path.join(__dirname, 'build')))
+app.get("/ping", (req, res) => {
+  return res.send("pong");
+});
 
-app.get('/ping', (req, res) => {
-  return res.send('pong')
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
-
-app.post('/mint', async (req, res) => {
+app.post("/mint", async (req, res) => {
   try {
-    console.log(req.body);
-    const rv = await TreasurContract.methods.approveMint(req.tokenUri, req.tokenURIStr, req.tokenCreator).send({"from": "0xd1058ECCEE8102Bb8C1A7390b7d6Ea2CB6dA8E0e"})
+    const {tokenUri, tokenURIStr, tokenCreator} = req.body;
+    const rv = await TreasurContract.methods
+      .approveMint(tokenUri, tokenURIStr, tokenCreator)
+      .send({from: "0xd1058ECCEE8102Bb8C1A7390b7d6Ea2CB6dA8E0e"});
     res.send(rv);
   } catch (e) {
-    console.log('This request is fucking up')
+    console.error(e)
   }
-
-})
-app.listen(8080)
+});
+app.listen(8080);
