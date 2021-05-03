95b36e4ba5bbad6c66fe84f083809f745364b793
Sarthak Kundra <sarthakkundra21@gmail.com>
2021-05-03 16:39:39 +0530

    almost set up YT API

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import {
   Grid,
   GridItem,
@@ -18,6 +18,16 @@ import BidButton from '../BidButton';
 // TODO: Get URL, Price, Name as props
 // Make all the text generic
 const Card = ({ URI }) => {
+
+  const[videoData, setVideoData] = useState(null);
+
+  useEffect(async () => {
+      // await loadClient();
+      const res = await axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&id=${URI}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`)
+      setVideoData(res.data.items[0]);
+     console.log(res.data.items[0]);
+  },[])
+
   return (
     <div>
       <Fade cascade>
@@ -40,7 +50,7 @@ const Card = ({ URI }) => {
           <GridItem colSpan={2}>
             <VStack align="stretch">
               <Text fontSize="2xl" isTruncated>
-                Highest in the Room
+                {videoData ? videoData.snippet.localized.title : 'Loading'}
               </Text>
               <Zoom>
                 <Text>
@@ -55,13 +65,13 @@ const Card = ({ URI }) => {
                 <IoMdThumbsUp />
               </Box>
               <Box w="50px" h="40px">
-                998k
+                {videoData ? videoData.statistics.likeCount : 'NA'}
               </Box>
               <Box style={{marginTop: "8px"}} w="20px" h="40px">
                 <IoMdThumbsDown />
               </Box>
               <Box w="50px" h="40px">
-                35k
+                {videoData ? videoData.statistics.dislikeCount : 'NA'}
               </Box>
             </HStack>
           </GridItem>
@@ -72,7 +82,7 @@ const Card = ({ URI }) => {
               </Box>
               <VStack align="stretch">
                 <Box w="150px" h="20px">
-                  <Text>Travis Scott</Text>
+                  <Text>{videoData ? videoData.snippet.channelTitle : "Loading"}</Text>
                 </Box>
                 <Box w="150px" h="20px">
                   <Text>4.96M</Text>
